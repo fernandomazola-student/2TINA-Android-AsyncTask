c26308b3521956e2dfccc24c2ee1e7e055e7da73
Fernando Mazola <fernando_mazola@hotmail.com>
2017-04-26 10:40:49 -0300

    Referenciando os componentes

--- app/src/main/java/br/com/fiap/a2tina_android_asynctask/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_asynctask/MainActivity.java
@@ -1,13 +1,23 @@
 package br.com.fiap.a2tina_android_asynctask;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String IMG_URL = "https://www.douglascabral.com.br/aula-api/img/01.png";
+    private ProgressDialog mProgressDialog;
+    private ImageView imgBaixada;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgBaixada = (ImageView) findViewById(R.id.imgBaixada);
+
     }
 }
